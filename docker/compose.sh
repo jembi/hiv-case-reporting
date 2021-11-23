1a984d1c8b9549d4962f527d7242a7f3043a492a
MattyJ007 <matthew.dickie@jembi.org>
2021-11-23 14:31:02 +0200

    Remove duplicated importer container from docker-compose
    The importer containers should only run on init not up. Config importers
    therefore need to remain in the docker-compose.config.yml only and the
    compose.sh file should implement the config only on init
    
    DIO-106

--- docker/compose.sh
+++ docker/compose.sh
@@ -18,15 +18,16 @@ if [ "$1" == "init" ]; then
     docker cp "$composeFilePath"/importer/logstash.yml logstash-helper:/config/
     docker cp "$composeFilePath"/importer/pipelines.yml logstash-helper:/config/
     docker rm logstash-helper
-
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
+    sleep 5
     docker restart logstash
+
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
 elif [ "$1" == "up" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 elif [ "$1" == "down" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml stop
 elif [ "$1" == "destroy" ]; then
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml down -v
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml down -v
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
